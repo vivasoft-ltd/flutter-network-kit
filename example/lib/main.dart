38365f6f8c6225bb430015a4f6490f14cfe12fac
mustafiz <m.rahman@vivasoftltd.com>
2023-04-06 22:45:26 +0600

    Usage added in demo for connectivity checker of network executor

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,7 +16,7 @@ Future<void> testGetApi() async {
   final value =
       await dioNetworkCallExecutor.execute<BaseError, List<Post>, Post>(
           options: RequestOptions(
-            headers: {
+    headers: {
       'Content-Type': 'application/json',
     },
     method: 'GET',
@@ -35,7 +35,7 @@ Future<void> testPostApi() async {
   final value = await dioNetworkCallExecutor
       .execute<BaseError, Map<String, dynamic>, Map<String, dynamic>>(
           options: RequestOptions(
-            headers: {
+    headers: {
       'Content-Type': 'application/json',
     },
     method: 'Post',
@@ -80,5 +80,88 @@ Future<void> jsonSerializableWay() async {
 }
 
 void main() async {
-  await jsonSerializableWay();
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MainScreen(),
+  ));
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  late final DioNetworkCallExecutor _dioNetworkCallExecutor;
+  bool isSubscribed = false;
+
+  @override
+  void initState() {
+    JsonSerializer jsonSerializer = JsonSerializer();
+    jsonSerializer.addParser<Post>(Post.fromJson);
+
+    _dioNetworkCallExecutor = DioNetworkCallExecutor(
+      dio: Dio(BaseOptions(baseUrl: 'https://jsonplaceholder.typicode.com')),
+      dioSerializer: jsonSerializer,
+      errorConverter: DioErrorToApiErrorConverter(),
+    );
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Network library demo'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                await jsonSerializableWay();
+              },
+              child: const Text('Initiate network call'),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                if (isSubscribed) {
+                  await _dioNetworkCallExecutor
+                      .unsubscribeFromConnectivityChange();
+                } else {
+                  _dioNetworkCallExecutor.subscribeToConnectivityChange(
+                    (connectivityResult) =>
+                        _showSnackBar(connectivityResult.isConnected()),
+                  );
+                }
+
+                setState(() {
+                  isSubscribed = !isSubscribed;
+                });
+              },
+              child: Text(
+                  '${isSubscribed ? 'Unsubscribe' : 'Subscribe'} to connectivity listener'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _showSnackBar(bool isConnected) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('You are ${isConnected ? 'online' : 'offline'}'),
+      ),
+    );
+  }
 }
